095466c6efc6438a0d62719763be6702b1b24379
AdityaAryan-1408 <aryanaditya1486@gmail.com>
2026-01-06 02:43:18 +0530

    Formatting

--- Solutions/2026-01-05_GeeksForGeeks_Max_Sum_Subarray_of_size_K.cpp
+++ Solutions/2026-01-05_GeeksForGeeks_Max_Sum_Subarray_of_size_K.cpp
@@ -4,8 +4,10 @@ Problem Name   : Max Sum Subarray of size K
 Platform       : GeeksForGeeks
 Topic          : Array, Sliding Window
 Problem Link   :
-https://www.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1 Type :
-POTD Difficulty     : Easy Date Solved    : 2026-01-05
+https://www.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1 
+Type           :POTD 
+Difficulty     : Easy 
+Date Solved    : 2026-01-05
 
 */
 
