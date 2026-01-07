4b8e792764c92367c901f8ba5a49a2742e6f4ab1
AdityaAryan-1408 <aryanaditya1486@gmail.com>
2026-01-07 22:10:23 +0530

    Solved GeeksForGeeks: K Sized Subarray Maximum [Array, Deque]

---
+++ Solutions/2026-01-07_GeeksForGeeks_K_Sized_Subarray_Maximum.cpp
@@ -0,0 +1,41 @@
+/*
+
+Problem Name   : K Sized Subarray Maximum
+Platform       : GeeksForGeeks
+Topic          : Array, Deque
+Problem Link   : https://www.geeksforgeeks.org/problems/maximum-of-all-subarrays-of-size-k3101/1?page=1&status=unsolved&sortBy=submissions
+Type           : Practice
+Difficulty     : Medium
+Date Solved    : 2026-01-07
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+   public:
+    vector<int> maxOfSubarrays(vector<int>& arr, int k) {
+        int n = arr.size();
+        vector<int> ans;
+        deque<int> dq;
+
+        for (int j = 0; j < n; j++) {
+            while (!dq.empty() && arr[dq.back()] <= arr[j]) dq.pop_back();
+
+            dq.push_back(j);
+
+            if (dq.front() <= j - k) dq.pop_front();
+
+            if (j >= k - 1) ans.push_back(arr[dq.front()]);
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    Solution s;
+
+    return 0;
+}
