ec33a558f9effc85cd09b5426554ba82bc823428
AdityaAryan-1408 <aryanaditya1486@gmail.com>
2026-01-19 14:04:48 +0530

    Solved LeetCode: Maximum Side Length of a Square with Sum Less than or Equal to Threshold [Array, Binary Search, PrefixSum]

---
+++ Solutions/2026-01-19_LeetCode_Maximum_Side_Length_of_a_Square_with_Sum_Less_than_or_Equal_to_Threshold.cpp
@@ -0,0 +1,59 @@
+/*
+
+Problem Name   : Maximum Side Length of a Square with Sum Less than or Equal to Threshold
+Platform       : LeetCode
+Topic          : Array, Binary Search, PrefixSum
+Problem Link   : https://leetcode.com/problems/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/description/?envType=daily-question&envId=2026-01-19
+Type           : POTD
+Difficulty     : Medium
+Date Solved    : 2026-01-19
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+   public:
+    int getRect(const vector<vector<int>>& P, int x1, int y1, int x2, int y2) {
+        return P[x2][y2] - P[x1 - 1][y2] - P[x2][y1 - 1] + P[x1 - 1][y1 - 1];
+    }
+
+    int maxSideLength(vector<vector<int>>& mat, int threshold) {
+        int m = mat.size(), n = mat[0].size();
+        vector<vector<int>> P(m + 1, vector<int>(n + 1));
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                P[i][j] = P[i - 1][j] + P[i][j - 1] - P[i - 1][j - 1] +
+                          mat[i - 1][j - 1];
+            }
+        }
+
+        int l = 1, r = min(m, n), ans = 0;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            bool find = false;
+            for (int i = 1; i <= m - mid + 1; ++i) {
+                for (int j = 1; j <= n - mid + 1; ++j) {
+                    if (getRect(P, i, j, i + mid - 1, j + mid - 1) <=
+                        threshold) {
+                        find = true;
+                    }
+                }
+            }
+            if (find) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution s;
+
+    return 0;
+}
