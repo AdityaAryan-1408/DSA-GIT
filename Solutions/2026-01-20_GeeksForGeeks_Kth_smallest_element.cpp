8a18f4d544363879c8da9f4658a77d41facc2066
AdityaAryan-1408 <aryanaditya1486@gmail.com>
2026-01-20 16:45:58 +0530

    Solved GeeksForGeeks: Kth smallest element [Priority Queue]

---
+++ Solutions/2026-01-20_GeeksForGeeks_Kth_smallest_element.cpp
@@ -0,0 +1,34 @@
+/*
+
+Problem Name   : Kth smallest element
+Platform       : GeeksForGeeks
+Topic          : Priority Queue
+Problem Link   : https://www.geeksforgeeks.org/problems/kth-smallest-element5545-1587115620--102500/1?page=1&difficulty=Medium&status=unsolved&sortBy=accuracy
+Type           : Practice
+Difficulty     : Medium
+Date Solved    : 2026-01-20
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+   public:
+    int kthSmallest(vector<int>& arr, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (int x : arr) pq.push(x);
+
+        while (!pq.empty() && k != 1) {
+            pq.pop();
+            k--;
+        }
+        return pq.top();
+    }
+};
+
+int main() {
+    Solution s;
+
+    return 0;
+}
