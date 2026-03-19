d62fcc73c33887703488b43e61a58c12c75055dc
Aditya Aryan <aryanaditya1486@gmail.com>
2026-03-20 00:16:57 +0530

    Solved LeetCode: Find the Duplicate Number [Fast and Slow] [Array, Two Pointers, Binary Search, Bit Manipulation]

---
+++ Solutions/2026-03-19_LeetCode_Find_the_Duplicate_Number_Fast_and_Slow.cpp
@@ -0,0 +1,37 @@
+/*
+
+Problem Name   : Find the Duplicate Number
+Platform       : LeetCode
+Topic          : Array, Two Pointers, Binary Search, Bit Manipulation
+Problem Link   : https://leetcode.com/problems/find-the-duplicate-number/submissions/1953291921/
+Type           : Practice
+Difficulty     : Medium
+Date Solved    : 2026-03-19
+Approach       : Fast and Slow
+Time           : O(N)
+Space          : O(1)
+
+*/
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = 0, fast = 0;
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        int slow2 = 0;
+        while (true) {
+            slow = nums[slow];
+            slow2 = nums[slow2];
+            if (slow == slow2) {
+                return slow;
+            }
+        }
+    }
+};
