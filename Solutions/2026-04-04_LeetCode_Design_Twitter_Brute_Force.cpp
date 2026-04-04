9becaa0a1b4033d519c6bf63af21080680deee0f
Aditya Aryan <aryanaditya1486@gmail.com>
2026-04-04 22:56:46 +0530

    Solved LeetCode: Design Twitter [Brute Force] [Hash Table, Linked List, Design, Heap (Priority Queue)]

---
+++ Solutions/2026-04-04_LeetCode_Design_Twitter_Brute_Force.cpp
@@ -0,0 +1,50 @@
+/*
+
+Problem Name   : Design Twitter
+Platform       : LeetCode
+Topic          : Hash Table, Linked List, Design, Heap (Priority Queue)
+Problem Link   : https://leetcode.com/problems/design-twitter/submissions/1968818808/
+Type           : Practice
+Difficulty     : Medium
+Date Solved    : 2026-04-04
+Approach       : Brute Force
+
+*/
+
+class Twitter {
+    int time;
+    unordered_map<int, unordered_set<int>> followMap;
+    unordered_map<int, vector<pair<int, int>>> tweetMap;
+public:
+    Twitter() : time(0) {}
+
+    void postTweet(int userId, int tweetId) {
+        tweetMap[userId].push_back({time++, tweetId});
+    }
+
+    vector<int> getNewsFeed(int userId) {
+        vector<pair<int, int>> feed = tweetMap[userId];
+        for (int followeeId : followMap[userId]) {
+            feed.insert(feed.end(), tweetMap[followeeId].begin(),
+                                    tweetMap[followeeId].end());
+        }
+        sort(feed.begin(), feed.end(), [](auto &a, auto &b) {
+            return a.first > b.first;
+        });
+        vector<int> res;
+        for (int i = 0; i < min(10, (int)feed.size()); ++i) {
+            res.push_back(feed[i].second);
+        }
+        return res;
+    }
+
+    void follow(int followerId, int followeeId) {
+        if (followerId != followeeId) {
+            followMap[followerId].insert(followeeId);
+        }
+    }
+
+    void unfollow(int followerId, int followeeId) {
+        followMap[followerId].erase(followeeId);
+    }
+};
