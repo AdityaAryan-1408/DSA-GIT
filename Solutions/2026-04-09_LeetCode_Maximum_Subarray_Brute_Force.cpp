0e9c9f0aa962a0996ce4299ffeb81312f8d8ab1c
Aditya Aryan <aryanaditya1486@gmail.com>
2026-04-09 20:03:11 +0530

    Solved LeetCode: Maximum Subarray [Brute Force] [Array, Divide and Conquer, Dynamic Programming]

---
+++ Solutions/2026-04-09_LeetCode_Maximum_Subarray_Brute_Force.cpp
@@ -0,0 +1,29 @@
+/*
+
+Problem Name   : Maximum Subarray
+Platform       : LeetCode
+Topic          : Array, Divide and Conquer, Dynamic Programming
+Problem Link   : https://leetcode.com/problems/maximum-subarray/submissions/1973604744/
+Type           : Practice
+Difficulty     : Medium
+Date Solved    : 2026-04-09
+Approach       : Brute Force
+Time           : O(N)
+Space          : O(1)
+
+*/
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size(), res = nums[0];
+        for (int i = 0; i < n; i++) {
+            int cur = 0;
+            for (int j = i; j < n; j++) {
+                cur += nums[j];
+                res = max(res, cur);
+            }
+        }
+        return res;
+    }
+};
