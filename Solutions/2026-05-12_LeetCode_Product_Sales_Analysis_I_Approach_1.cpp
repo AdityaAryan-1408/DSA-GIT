f94f2015b7c286b024cedd7035bc6dc304e99649
Aditya Aryan <aryanaditya1486@gmail.com>
2026-05-12 16:32:39 +0530

    Solved LeetCode: Product Sales Analysis I [Approach 1] [Database, SQL50]

---
+++ Solutions/2026-05-12_LeetCode_Product_Sales_Analysis_I_Approach_1.cpp
@@ -0,0 +1,19 @@
+/*
+
+Problem Name   : Product Sales Analysis I
+Platform       : LeetCode
+Topic          : Database, SQL50
+Problem Link   : https://leetcode.com/problems/product-sales-analysis-i/submissions/2001270823/
+Type           : Practice
+Difficulty     : Easy
+Date Solved    : 2026-05-12
+Approach       : Approach 1
+
+*/
+
+SELECT  p.product_name,  s.year, s.price
+FROM Sales AS s
+LEFT JOIN
+Product as p
+ON
+s.product_id = p.product_id;
