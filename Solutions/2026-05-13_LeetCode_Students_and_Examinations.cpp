ffe5df07a0215d642d5f7eb529dda62fc9b750a8
Aditya Aryan <aryanaditya1486@gmail.com>
2026-05-13 12:39:54 +0530

    Solved LeetCode: Students and Examinations [Database, SQL50]

---
+++ Solutions/2026-05-13_LeetCode_Students_and_Examinations.cpp
@@ -0,0 +1,25 @@
+/*
+
+Problem Name   : Students and Examinations
+Platform       : LeetCode
+Topic          : Database, SQL50
+Problem Link   : https://leetcode.com/problems/students-and-examinations/submissions/2001941265/
+Type           : Practice
+Difficulty     : Easy
+Date Solved    : 2026-05-13
+
+*/
+
+SELECT s.student_id, s.student_name,su.subject_name, COUNT(e.student_id) as attended_exams
+FROM
+Students as s
+CROSS JOIN
+Subjects as su
+LEFT JOIN
+Examinations as e
+ON
+s.student_id = e.student_id AND
+su.subject_name = e.subject_name
+
+GROUP BY s.student_id, s.student_name, su.subject_name
+ORDER BY s.student_id, su.subject_name
