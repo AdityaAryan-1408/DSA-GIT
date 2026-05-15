e7429e5ed97267e8cc4c8aa78a50d596e9b488d6
Aditya Aryan <aryanaditya1486@gmail.com>
2026-05-15 16:11:56 +0530

    Solved LeetCode: House Robber [Tabulation] [Array, Dynamic Programming]

---
+++ Solutions/2026-05-15_LeetCode_House_Robber_Tabulation.cpp
@@ -0,0 +1,40 @@
+/*
+
+Problem Name   : House Robber
+Platform       : LeetCode
+Topic          : Array, Dynamic Programming
+Problem Link   : https://leetcode.com/problems/house-robber/submissions/2003720947/
+Type           : Practice
+Difficulty     : Medium
+Date Solved    : 2026-05-15
+Approach       : Tabulation
+Time           : O(N)
+Space          : O(1)
+
+*/
+
+class Solution {
+public:
+    int n;
+    int helper(vector<int>& nums, vector<int>& dp) {
+        int prev = nums[0];
+        int prev2 = 0, curr;
+        int pick, notPick;
+        for (int i = 1; i < n; i++) {
+            pick = nums[i];
+            if (i > 1)
+                pick += prev2;
+
+            notPick = prev;
+           curr = max(pick, notPick);
+           prev2 = prev;
+           prev = curr;
+        }
+        return prev;
+    }
+    int rob(vector<int>& nums) {
+        n = nums.size();
+        vector<int> dp(n, -1);
+        return helper(nums, dp);
+    }
+};
