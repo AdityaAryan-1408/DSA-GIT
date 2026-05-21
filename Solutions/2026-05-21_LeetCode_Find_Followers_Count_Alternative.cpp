a7aa28242bf2a1cb741ce40240188b7d2a3dd002
Aditya Aryan <aryanaditya1486@gmail.com>
2026-05-21 15:56:46 +0530

    Solved LeetCode: Find Followers Count [Alternative] [Database]

---
+++ Solutions/2026-05-21_LeetCode_Find_Followers_Count_Alternative.cpp
@@ -0,0 +1,16 @@
+/*
+
+Problem Name   : Find Followers Count
+Platform       : LeetCode
+Topic          : Database
+Problem Link   : https://leetcode.com/problems/find-followers-count/submissions/2008935531/
+Type           : Practice
+Difficulty     : Easy
+Date Solved    : 2026-05-21
+Approach       : Alternative
+
+*/
+
+SELECT user_id, COUNT(DISTINCT follower_id) AS followers_count
+FROM Followers
+GROUP BY user_id
