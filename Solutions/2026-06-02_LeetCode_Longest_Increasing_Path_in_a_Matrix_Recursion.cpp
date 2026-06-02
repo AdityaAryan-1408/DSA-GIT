9a17de447d5bf47d63607d01fbe6c789fb143da5
Aditya Aryan <aryanaditya1486@gmail.com>
2026-06-02 13:09:51 +0530

    Solved LeetCode: Longest Increasing Path in a Matrix [Recursion] [Array, Dynamic Programming, Depth-First Search, Breadth-First Search, Graph Theory, Topological Sort, Memoization, Matrix]

---
+++ Solutions/2026-06-02_LeetCode_Longest_Increasing_Path_in_a_Matrix_Recursion.cpp
@@ -0,0 +1,42 @@
+/*
+
+Problem Name   : Longest Increasing Path in a Matrix
+Platform       : LeetCode
+Topic          : Array, Dynamic Programming, Depth-First Search, Breadth-First Search, Graph Theory, Topological Sort, Memoization, Matrix
+Problem Link   : https://leetcode.com/problems/longest-increasing-path-in-a-matrix/submissions/2019900609/
+Type           : Practice
+Difficulty     : Hard
+Date Solved    : 2026-06-02
+Approach       : Recursion
+Time           : O(N * M)
+Space          : O(N * M)
+
+*/
+
+class Solution {
+public:
+    int n, m;
+    int path(vector<vector<int>>& mat, int i, int j, int prev) {
+        if (i >= n || i < 0 || j >= m || j < 0)
+            return 0;
+        if (mat[i][j] <= prev)
+            return 0;
+
+        int up = 1 + path(mat, i - 1, j, mat[i][j]);
+        int down = 1 + path(mat, i + 1, j, mat[i][j]);
+        int left = 1 + path(mat, i, j - 1, mat[i][j]);
+        int right = 1 + path(mat, i, j + 1, mat[i][j]);
+        return max({up, down, left, right});
+    }
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        n = matrix.size(), m = matrix[0].size();
+        int maxDist = INT_MIN;
+        for(int i = 0; i <n; i++){
+            for(int j = 0; j< m; j++){
+                int dist = path(matrix, i, j, INT_MIN);
+                maxDist = max(maxDist, dist);
+            }
+        }
+        return maxDist;
+    }
+};
