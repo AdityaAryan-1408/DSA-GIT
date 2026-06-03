c75bdab6935193376825a52e8685e2985ef50d48
Aditya Aryan <aryanaditya1486@gmail.com>
2026-06-03 14:40:09 +0530

    Solved LeetCode: Distinct Subsequences [Space Optimized 1D array] [String, Dynamic Programming]

---
+++ Solutions/2026-06-03_LeetCode_Distinct_Subsequences_Space_Optimized_1D_array.cpp
@@ -0,0 +1,34 @@
+/*
+
+Problem Name   : Distinct Subsequences
+Platform       : LeetCode
+Topic          : String, Dynamic Programming
+Problem Link   : https://leetcode.com/problems/distinct-subsequences/
+Type           : Practice
+Difficulty     : Hard
+Date Solved    : 2026-06-03
+Approach       : Space Optimized 1D array
+Time           : O(N * M)
+Space          : O(N * M)
+
+*/
+
+class Solution {
+public:
+    int numDistinct(string& s, string& t) {
+        int n = s.size(), m = t.size();
+        vector<double> prev(m + 1, 0);
+        prev[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = m; j>= 1; j--) {
+                if (s[i - 1] == t[j - 1]) {
+                    prev[j] = prev[j - 1] + prev[j];
+                } else {
+                    prev[j] = prev[j];
+                }
+            }
+        }
+        return (int)prev[m];
+    }
+};
