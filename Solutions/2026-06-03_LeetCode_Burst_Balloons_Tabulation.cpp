d03d2f43858f1fa5d8646e63cbb56edc6dc37e1b
Aditya Aryan <aryanaditya1486@gmail.com>
2026-06-03 22:27:48 +0530

    Solved LeetCode: Burst Balloons [Tabulation] [Array, Dynamic Programming]

---
+++ Solutions/2026-06-03_LeetCode_Burst_Balloons_Tabulation.cpp
@@ -0,0 +1,38 @@
+/*
+
+Problem Name   : Burst Balloons
+Platform       : LeetCode
+Topic          : Array, Dynamic Programming
+Problem Link   : https://leetcode.com/problems/burst-balloons/
+Type           : Practice
+Difficulty     : Hard
+Date Solved    : 2026-06-03
+Approach       : Tabulation
+
+*/
+
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        nums.push_back(1);
+        nums.insert(nums.begin(), 1);
+
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n; j++) {
+                if (i > j)
+                    continue;
+
+                int maxCost = INT_MIN;
+                for (int ind = i; ind <= j; ind++) {
+                    int cost = nums[i - 1] * nums[ind] * nums[j + 1] +
+                               dp[i][ind - 1] + dp[ind + 1][j];
+                    maxCost = max(maxCost, cost);
+                }
+                dp[i][j] = maxCost;
+            }
+        }
+        return dp[1][n];
+    }
+};
