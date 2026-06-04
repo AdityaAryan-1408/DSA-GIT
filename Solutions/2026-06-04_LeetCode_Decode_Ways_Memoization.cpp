44f9f9d0a510d211537c50ed4c5396cdacec823a
Aditya Aryan <aryanaditya1486@gmail.com>
2026-06-04 14:19:19 +0530

    Solved LeetCode: Decode Ways [Memoization] [String, Dynamic Programming]

---
+++ Solutions/2026-06-04_LeetCode_Decode_Ways_Memoization.cpp
@@ -0,0 +1,46 @@
+/*
+
+Problem Name   : Decode Ways
+Platform       : LeetCode
+Topic          : String, Dynamic Programming
+Problem Link   : https://leetcode.com/problems/decode-ways/submissions/2022078224/
+Type           : Practice
+Difficulty     : Medium
+Date Solved    : 2026-06-04
+Approach       : Memoization
+Time           : O(N)
+Space          : O(N)
+
+*/
+
+class Solution {
+public:
+    int n;
+    vector<int> dp;
+    int helper(int i, string& s) {
+        if (i == n) {
+            return 1;
+        }
+
+        if (s[i] == '0')
+            return 0;
+
+        if (dp[i] != -1)
+            return dp[i];
+
+        int onlyI = helper(i + 1, s);
+
+        int iplus1 = 0;
+        if (i + 1 < n) {
+            if (s[i] == '1' || s[i] == '2' && s[i + 1] <= '6') {
+                iplus1 = helper(i + 2, s);
+            }
+        }
+        return dp[i] = onlyI + iplus1;
+    }
+    int numDecodings(string s) {
+        n = s.size();
+        dp.assign(n, -1);
+        return helper(0, s);
+    }
+};
